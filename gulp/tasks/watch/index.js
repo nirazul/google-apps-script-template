645bc80aa69c771417725ae02067cacceb245ca1
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2018-02-13 11:45:13 +0100

    Add watch:all task

--- gulp/tasks/watch/index.js
+++ gulp/tasks/watch/index.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const gulp = require('gulp');
+const { spawn } = require('child_process');
 const { debounce } = require('lodash');
 
-const { throwOnInvalidTargetProject } = require('../../util/target-project');
-const { claspPush, compileJs, copy } = require('../../config/task-configs');
 const { createFileWatchers } = require('./helpers');
+const paths = require('../../config/paths');
+const projects = require('../../config/projects');
+const { claspPush, compileJs, copy } = require('../../config/task-configs');
+const { throwOnNoRegisteredProjects } = require('../../util/registered-projects');
+const { throwOnInvalidTargetProject } = require('../../util/target-project');
 
 /**
  * Add file watchers for all tasks
@@ -15,17 +19,32 @@ function watch() {
   const watchers = createFileWatchers({ claspPush, compileJs, copy });
 
   // Javascript task files
-  watchers.compileJs
-      .on('all', debounce(gulp.series('compile-js'), 200));
+  watchers.compileJs.on('all', debounce(gulp.series('compile-js'), 200));
 
   // Copy task files
-  watchers.copy
-      .on('all', debounce(gulp.series('copy'), 200));
+  watchers.copy.on('all', debounce(gulp.series('copy'), 200));
 
   // Dist file upload
-  watchers.claspPush
-      .on('all', debounce(gulp.series('clasp-push'), 1000));
+  watchers.claspPush.on('all', debounce(gulp.series('clasp-push'), 1000));
+}
+
+/**
+ * Start file watchers for all registered projects
+ */
+function watchAll() {
+  throwOnNoRegisteredProjects('watch:all');
+
+  projects.forEach(project => spawn('gulp', ['watch:only', '--project-dir', project], {
+    cwd: paths.root,
+    stdio: 'inherit',
+  }));
 }
 
 // NOTE: Main task
 gulp.task('watch', gulp.series('build', watch));
+
+// NOTE: Watch without building beforehand
+gulp.task('watch:only', watch);
+
+// NOTE: Watch all projets
+gulp.task('watch:all', gulp.series('build:all', watchAll));
