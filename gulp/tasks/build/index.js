54aa08709a599a97cde811de04670859fbf75a25
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2018-02-13 11:06:41 +0100

    Add build:all task

--- gulp/tasks/build/index.js
+++ gulp/tasks/build/index.js
@@ -1,4 +1,9 @@
 const gulp = require('gulp');
+const { spawn } = require('child_process');
+
+const paths = require('../../config/paths');
+const projects = require('../../config/projects');
+const { throwOnNoRegisteredProjects } = require('../../util/registered-projects');
 
 // NOTE: Inits a full build
 gulp.task('build', gulp.series(
@@ -6,3 +11,24 @@ gulp.task('build', gulp.series(
     gulp.parallel('compile-js', 'copy'),
     'clasp-push',
 ));
+
+gulp.task('build:all', function(done) {
+  throwOnNoRegisteredProjects('build:all');
+
+  const promises = projects.map(project => () => new Promise((resolve, reject) => {
+    const child = spawn('gulp', ['build', '--project-dir', project], {
+      cwd: paths.root,
+      stdio: 'inherit',
+    });
+
+    child.on('close', code => {
+      if (!code) {
+        resolve();
+      } else {
+        reject(code);
+      }
+    });
+  }));
+
+  return promises.reduce((acc, promise) => acc.then(promise), Promise.resolve());
+});
