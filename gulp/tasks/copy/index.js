d59b3d5c76d10b87a00c1647368799109065cd03
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2018-06-03 15:46:41 +0200

    Add copy / copy:clean / copy:dirty tasks

--- gulp/tasks/copy/index.js
+++ gulp/tasks/copy/index.js
@@ -8,10 +8,16 @@ const config = require('./config');
  * @param {Function} resolve - A completion indicator callback
  */
 function copy(resolve) {
-  return gulp.src(config.src.globs, config.src.options)
-      .pipe(plumberPipe())
-      .pipe(gulp.dest(config.dest.path));
+    return gulp.src(config.src.globs, config.src.options)
+        .pipe(plumberPipe())
+        .pipe(gulp.dest(config.dest.path));
 }
 
-// NOTE: Invoke copy task
-gulp.task('copy', copy);
+// NOTE: Alias for `copy:clean`
+gulp.task('copy', gulp.series('copy:clean'));
+
+// NOTE: Run main task, after cleaning up older files
+gulp.task('copy:clean', gulp.series('clean:copy', 'copy:dirty'));
+
+// NOTE: Main task
+gulp.task('copy:dirty', copy);
