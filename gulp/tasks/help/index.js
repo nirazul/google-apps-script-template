1bf31486727454ceff73229c6419d1789ecd691e
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2018-06-04 19:40:30 +0200

    Streamline console usage

--- gulp/tasks/help/index.js
+++ gulp/tasks/help/index.js
@@ -1,11 +1,12 @@
 const gulp = require('gulp');
+const fancyLog = require('fancy-log');
 
 /**
  * Give additional information to the available tasks
  * @param {Function} resolve - A completion indicator callback
  */
 function help(resolve) {
-    console.log('Please use `gulp --tasks` for a list of all available tasks!');
+    fancyLog.info('⚠️  Please use `gulp --tasks` for a list of all available tasks!');
     resolve();
 }
 
