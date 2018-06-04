63cb6eea10bbabdb98f33dfa6849b4e6fc8908cb
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2018-06-04 19:33:02 +0200

    Fix undefined glob error

--- gulp/config/paths.js
+++ gulp/config/paths.js
@@ -4,5 +4,5 @@ const { types, name, assetsDirName } = require('../util/env');
 const { 'project-dir': projectDir } = require('../util/args');
 
 const root = exports.root = path.resolve(__dirname, '..', '..');
-const src = exports.src = (projectDir ? path.join(root, projectDir, 'src') : null);
-const dest = exports.dest = (projectDir ? path.join(root, projectDir, 'dist') : null);
+const src = exports.src = (projectDir ? path.join(root, projectDir, 'src') : root);
+const dest = exports.dest = (projectDir ? path.join(root, projectDir, 'dist') : root);
