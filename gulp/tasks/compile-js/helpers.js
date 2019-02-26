7ba8163f1a7f3c85dbc0f3c43bfcbbbce34a4981
nirazul <rouven.buehlmann@dreipol.ch>
2019-02-26 18:45:15 +0100

    Remove unused files

--- gulp/tasks/compile-js/helpers.js
+++ gulp/tasks/compile-js/helpers.js
@@ -3,7 +3,7 @@ const fancyLog = require('fancy-log');
 
 /**
  * Create a path that is relative to the src directory to use in globbing patterns with a `cwd`
- * @param {Object} paths - The paths object
+ * @param {object} paths - The paths object
  * @param {string} paths.root - The root path
  * @param {string} paths.src - The src path
  * @param {string} dir - The project directory path
@@ -16,7 +16,7 @@ exports.getSourceRelativeDir = function({ root, src }, dir) {
 /**
  * Output errors that come from the webpack bundler
  * @param {Error} error - A webpack error object
- * @param {Object} stats - A webpack stats object
+ * @param {object} stats - A webpack stats object
  */
 exports.outputBundlerStats = function(error, stats) {
     if (error) {
--- gulp/util/gulp-plugins.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    lazy: true,
-};
-
-module.exports = require('gulp-load-plugins')(config);
